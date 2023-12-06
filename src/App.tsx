a882e20d11215625e7f6ca658e9153f7095af318
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2023-12-07 00:54:06 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import  MasterComponent  from "./backend/layouts/master";
+
+function App() {
+  return (
+    <MasterComponent/>
+  )
+}
+
+export default App;
